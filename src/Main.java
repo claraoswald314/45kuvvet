badc22cdf1f44747c68cbbc5b42ea2d47f9a3c89
claraoswald314 <86008186+claraoswald314@users.noreply.github.com>
2023-01-20 04:18:52 +0300

    patika.dev in java 101 derslerinin 4 ve 5 kuvvetlerini yazdırma ödevidir
    patika.dev in java 101 derslerinin 4 ve 5 kuvvetlerini yazdırma ödevidir

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sınır sayısını giriniz : ");
+        n= input.nextInt();
+
+        for(int i=1; i<=n; i*=4){
+            System.out.println("dördün kuvveti : " + i);
+
+        }
+        for(int i=1; i<=n; i*=5){
+            System.out.println("beşin kuvveti : " + i);
+
+        }
+
+
+
+    }
+
+
+
+
+}
